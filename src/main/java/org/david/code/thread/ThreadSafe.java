3d306b19d1a2cd71742eaf38baf14d4cff9416fc
david <david_bdj@163.com>
2017-06-08 23:46:51 +0800

    增加线程安全相关代码

---
+++ src/main/java/org/david/code/thread/ThreadSafe.java
@@ -0,0 +1,36 @@
+package org.david.code.thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by david on 2017/6/8.
+ */
+public class ThreadSafe {
+
+    private static AtomicInteger count = new AtomicInteger(0);
+
+    public void testAtomicInteger() {
+        int i1 = count.get();
+        System.out.println(i1);
+        int i2 = count.getAndSet(5);
+        System.out.println(i2);
+        int i3 = count.get();
+        System.out.println(i3);
+        int i4 = count.getAndIncrement();
+        System.out.println(i4);
+        int i5 = count.get();
+        System.out.println(i5);
+        int i6 = count.incrementAndGet();
+        System.out.println(i6);
+        int i7 = count.get();
+        System.out.println(i7);
+
+        boolean b1 = count.compareAndSet(7, 1);//先预期当前值，如果true才set
+        System.out.println("b1=" + b1 + ",count=" + count.get());
+
+
+    }
+
+}
---
+++ src/test/java/org/david/code/thread/ThreadSafeTest.java
@@ -0,0 +1,36 @@
+package org.david.code.thread;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+/**
+ * ThreadSafe Tester.
+ *
+ * @author david
+ * @version 1.0
+ * @since <pre>06/08/2017</pre>
+ */
+public class ThreadSafeTest {
+
+    private ThreadSafe threadSafe;
+
+    @Before
+    public void before() throws Exception {
+        threadSafe = new ThreadSafe();
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: testAtomicInteger()
+     */
+    @Test
+    public void testTestAtomicInteger() throws Exception {
+        threadSafe.testAtomicInteger();
+    }
+
+
+} 
