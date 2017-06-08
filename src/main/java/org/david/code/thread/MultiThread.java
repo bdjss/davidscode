09a06b2dce1df15349788d7ebc3f0c13d5bbde45
zhangwei <weizhang208953@sohu-inc.com>
2017-06-08 14:03:40 +0800

    init

--- src/main/java/org/david/code/thread/MultiThread.java
+++ src/main/java/org/david/code/thread/MultiThread.java
@@ -1,15 +1,27 @@
 package org.david.code.thread;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by william on 2017/6/7.
  */
 public class MultiThread {
 
-    public static void main(String[] args) {
-        System.out.println(123);
+    private static Logger logger = LoggerFactory.getLogger(MultiThread.class);
+
+    private ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+    public void run() {
+        executorService.execute(new Runnable() {
+            public void run() {
+                System.out.println("123");
+                logger.info("running...");
+            }
+        });
     }
-
-
-
 
 }
